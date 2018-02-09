806215feb817f1a146d159a779352ec0e180980f
Jules GROSPEILLER <grospeiller.jules@live.fr>
2018-02-09 16:13:47 +0100

    modification de l'utilisateur de la BDD

--- API/api-messaj/src/main/java/com/messaj/api/api_messaj/dao/DBConnection.java
+++ API/api-messaj/src/main/java/com/messaj/api/api_messaj/dao/DBConnection.java
@@ -9,21 +9,11 @@ import com.mysql.cj.jdbc.MysqlDataSource;
 public class DBConnection {
 
 	public static Connection getConnection() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
-//		MysqlDataSource dataSource = new MysqlDataSource();
-//		dataSource.setUser("root");
-//		dataSource.setPassword("test");
-//		dataSource.setServerName("localhost");
-//		
-//		//jdbc:mysql://localhost/db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC
-//		return dataSource.getConnection();
-//		
-		
+
 		Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
 		String url = "jdbc:mysql://localhost/MessageDB?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-		return DriverManager.getConnection(url, "root", "test");
-		
-		
-		  
+		return DriverManager.getConnection(url, "user", "password");
+
 	}
 
 }
